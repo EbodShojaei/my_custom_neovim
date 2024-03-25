40bd1e7e31155e1902c667aeca562a51aa7feff3
Ebod Shojaei <ebod.shojaei@gmail.com>
2024-03-24 23:29:46 -0700

    Included safety imports for plugins

--- lua/dobe/plugins/lsp/lspconfig.lua
+++ lua/dobe/plugins/lsp/lspconfig.lua
@@ -6,6 +6,13 @@ if not lspconfig_status then
   return
 end
 
+-- import cmp-nvim-lsp plugin safely
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_status then
+  return
+end
+
+-- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
   return
@@ -15,6 +22,7 @@ mason.setup({
   ui = {icons = {package_installed = "✓", package_pending = "➜", package_uninstalled = "✗"}}
 })
 
+-- import mason-lspconfig safely
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
   return
@@ -22,13 +30,22 @@ end
 
 mason_lspconfig.setup()
 
+-- setup keymap options
 local keymap = vim.keymap
 -- keybind options
 local opts = { noremap = true, silent = true }
 -- require("luasnip.loaders.from_vscode").lazy_load()
-local cmp = require 'cmp'
+local cmp_status, cmp = pcall(require, 'cmp')
+if not cmp_status then
+  return
+end
+
 local select_opts = {behavior = cmp.SelectBehavior.Select}
-local lspkind = require('lspkind')
+local lspkind_status, lspkind = pcall(require, 'lspkind')
+if not lspkind_status then
+  return
+end
+
 cmp.setup({
   experimental = {ghost_text = true},
   formatting = {
@@ -98,10 +115,10 @@ cmp.setup.cmdline(':', {
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 -- capabilities.offsetEncoding = 'utf-8' -- disabled to remove python lsp error
 -- See :help mason-lspconfig-dynamic-server-setup
-require('mason-lspconfig').setup_handlers({
+mason_lspconfig.setup_handlers({
   function(server)
     lspconfig[server].setup {capabilities = capabilities}
   end,
