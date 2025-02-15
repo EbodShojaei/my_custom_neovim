9af2194cf0b9604feb8e46f5ef22c949891789cf
EbodShojaei <ebod.shojaei@alumni.ubc.ca>
2025-02-15 05:26:49 -0800

    (delete) goodbye v1.1.0 and hello v1.2.0 🚀

--- nvim-custom/lua/dobe/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local status, _ = pcall(vim.cmd, "colorscheme tokyonight") -- Protected call in case theme not installed
-if not status then
-	print("Colorscheme not found!")
-	return
-end
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
